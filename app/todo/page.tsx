0f16257ba477c9920c01622082e49806464424b2
Reossann <s1f102502283@iniad.org>
2025-11-24 19:35:20 +0900

    feat:todoリスト => 完成(追加、削除、完了)

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 type Todoitem = {
     id: number;
     text:string;
+    isDone:boolean;
 }
 
 
@@ -16,11 +17,29 @@ export default function TodoPage() {
 
     const handleAdd = () => {
         const new_list = structuredClone(list);
-        const add_elements = {text:input, id: Date.now()};
+        const add_elements = {text:input, id: Date.now(), isDone:false};
+        if (add_elements.text === "")return;
         new_list.push(add_elements);
-        setList(new_list)
-        setInput("")
-
+        setList(new_list);
+        setInput("");
+    }
+    const handleRemove = (id : number) => {
+      if (window.confirm("本当に消しますか？")){
+        const new_list = list.filter(element => {
+            return element.id != id
+        })
+        setList(new_list);
+      }else return;
+    }
+    
+    const handleCheck = (id:number) => {
+      const new_item = list.map((item) => {
+        if (item.id === id){
+          return { ...item, isDone: !item.isDone };
+        }
+        return item;
+      })
+      setList(new_item)
     }
   return (
     // min-h-screen: 画面の高さいっぱい, p-8: 余白
@@ -41,16 +60,47 @@ export default function TodoPage() {
           value={input} 
           onChange={(e) => setInput(e.target.value)}
           className="border p-2 rounded-md flex-1 text-black" 
+          onKeyDown={(e) => {
+            if (e.key == "Enter"){
+              handleAdd()
+            }
+          }}
         />
-        <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600" onClick={handleAdd}>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600" onClick={handleAdd} >
           追加
         </button>
       </div>
 
       <div className="space-y-2">
         {list.map((list_element) => (
-            <div key={list_element.id} className='bg-yellow-300 text-black px-4 py-2 rounded-md hover:bg-red-500'>
-                <h1>{list_element.text}</h1>
+            <div 
+            tabIndex={0}
+            key={list_element.id} 
+            className='flex bg-yellow-300 text-black px-4 py-2 rounded-md hover:bg-red-500'
+            onKeyDown={(e)=>{
+              if (e.key == "Backspace"){
+              handleRemove(list_element.id)
+            }
+            }}
+            >
+              <div className='flex gap-2 item-center'>
+                <input
+                type='checkbox'
+                checked={list_element.isDone}
+                onChange={() => handleCheck(list_element.id)}
+                className='w-10 h-10' />
+              </div>
+                <h1 className={`
+                ${list_element.isDone ? "text-gray-500 line-through" : ""} 
+                text-2xl 
+                mx-4`}>
+                  {list_element.text}
+                </h1>
+                <button
+                onClick={() => handleRemove(list_element.id)}
+                className='bg-blue-200 text-black px-2 py-1 rounded'>
+                  削除
+                </button>
             </div>
         ))}
       </div>
