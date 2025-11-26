6ff4475f1c9b3d02e2f47239ab56d96522a1c657
Reossann <s1f102502283@iniad.org>
2025-11-26 19:19:00 +0900

    feat: 電卓のかたちはできてきた

--- app/calc/page.tsx
+++ app/calc/page.tsx
@@ -3,7 +3,6 @@
 import Link from 'next/link'; // 忘れずに！
 import { useState } from 'react';
 import { useRouter } from 'next/navigation'; // ← 1. 追加！
-import { addTodo, deleteTodo, toggleTodo } from '../actions';
 
 
 
@@ -11,18 +10,68 @@ import { addTodo, deleteTodo, toggleTodo } from '../actions';
 
 export default function Calc(){
 
+    const numbersList = ["1","2","3","4","5","6","7","8","9","CRE","0","="];
+    const [result, setResult] = useState<string>("")
+    const handlePrint = (number: string) => {
+        let new_result = structuredClone(result);
+        if ("CRE" === number){
+                new_result = ""
+                setResult(new_result)
+                return
+            }
+        if ("=" === number){
+
+        }
+        if ("+" === number)
+        for (const i of numbersList){
+            
+            if (number === i){
+                new_result += number
+                setResult(new_result)
+                return
+            }
+            
+        }
+
+        
+        
+    }
+
 
 
     return(
-        <main className="min-h-screen p-8 bg-gray-50">
+        <main className="
+        min-h-screen 
+        p-8 
+        bg-gray-50 center
+        min-h-screen 
+         ">
             <div className="mb-8">
                 <Link href="/" className="text-blue-500 hover:underline text-2xl">
           ← ホームに戻る
                 </Link>
             </div>
-            <h1 className='text-5xl center'>
-                開発途中。。。。
+            <h1 
+            className='text-5xl center bg-blue-500 px-4 py-2'
+            >
+                {result}
             </h1>
+            <div className='
+            grid        
+            justify-center 
+            grid-cols-3
+            gap-2
+            '>
+            {numbersList.map((numbers) => (
+                
+                    <button 
+                    key={numbers}
+                    onClick={() => handlePrint(numbers)}
+                    className='text-8xl border border-blue-400'>
+                        {numbers}
+                    </button>
+            ))}
+            </div>
         </main>
     )
 }
