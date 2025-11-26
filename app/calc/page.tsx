334ba0c63dc5682334c057fb9a1127f7373dbbe4
Reossann <s1f102502283@iniad.org>
2025-11-26 13:25:11 +0900

    feat:電卓アプリのひな型作成

--- app/calc/page.tsx
+++ app/calc/page.tsx
@@ -1,2 +1,28 @@
 "use client";
 
+import Link from 'next/link'; // 忘れずに！
+import { useState } from 'react';
+import { useRouter } from 'next/navigation'; // ← 1. 追加！
+import { addTodo, deleteTodo, toggleTodo } from '../actions';
+
+
+
+
+
+export default function Calc(){
+
+
+
+    return(
+        <main className="min-h-screen p-8 bg-gray-50">
+            <div className="mb-8">
+                <Link href="/" className="text-blue-500 hover:underline text-2xl">
+          ← ホームに戻る
+                </Link>
+            </div>
+            <h1 className='text-5xl center'>
+                開発途中。。。。
+            </h1>
+        </main>
+    )
+}
