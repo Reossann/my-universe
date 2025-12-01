9bea8e2616b859797cf5453467dd5ec278f0e01f
Reossann <s1f102502283@iniad.org>
2025-12-02 08:24:43 +0900

    feat: 新しいアプリの種を作成

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ const apps = [
   { id: "todo", name: "Todo List", path: "/todo", description: "タスク管理アプリ", color:"#ffe4e1"},
 {id: "calc", name: "電卓", path: "/calc", description: "計算アプリ", color:"blue"},
 {id: "pachinco", name: "パチンコ確率機", path: "/pachinco", description: "パチンコ確率アプリ", color:"red"},
+{id: "SG", name: "ソシャゲの面白いところだけを集めたい", path: "/sg", description: "ソシャゲ濃縮アプリ", color:"green"},
 ]
 
 
