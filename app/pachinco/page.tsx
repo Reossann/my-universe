62be29d6eee82b4f775969f739dfa2865fc1c402
Reossann <s1f102502283@iniad.org>
2025-12-03 14:21:47 +0900

    feat: Pachinco => 確率ロジック完成

--- app/pachinco/page.tsx
+++ app/pachinco/page.tsx
@@ -2,30 +2,46 @@
 
 import Link from 'next/link'; // 忘れずに！
 import { useState } from 'react';
-import "random";
 
 
 export default function P(){
     const [counter, setCounter] = useState<number>(0);
+    const [result,setResult] = useState<string>("Yeear!!");
     const handleLack = () => {
+        if (result === "Yes!!!")return
         setCounter(counter + 1)
+        console.log(counter)
         const randomNumber = Math.random()
-        if (0.09 < randomNumber ){
-            
+        console.log(randomNumber)
+        if (randomNumber < 1/99.9){
+            setResult("Yes!!!")
+        }else{
+            setResult("No!!!")
         }
     }
 
 
 
-   return( <main>
+   return( <main 
+    tabIndex={0}
+    onKeyDown={(e) =>{
+            if(e.key === "Enter")handleLack()
+        }}>
         <div className="mb-8">
                 <Link href="/" className="text-blue-500 hover:underline text-2xl">
           ← ホームに戻る
                 </Link>
             </div>
         <h1 className='text-5xl center px-4 py-2'>
-            開発途中....!!!!!!
+            {result}
         </h1>
+        <div className='text-5xl'>
+            {counter}
+        </div>
+        <button 
+        className='bg-red border-4 border-gray-800' 
+        onClick={() => handleLack()}
+        >おせ！！！</button>
     </main>
    )
 }
